c8187a363f9d3ec0409e228b7740ecde71350d77
Martin Bridgman <martin.bridgman@gmail.com>
2022-02-02 23:40:52 +0000

    Jest Dom complete

--- scripts/test/dom.test.js
+++ scripts/test/dom.test.js
@@ -4,11 +4,7 @@
  const buttonClick = require("../button");
 
  beforeEach(() => {
-     let fs = require("fs");
-     let fileContents = fs.readFileSync("index.html", "utf-8");
-     document.open();
-     document.write(fileContents);
-     document.close();
+     document.body.innerHTML = "<p id='par'></p>";
  });
  
  describe("DOM tests", () => {
@@ -16,8 +12,5 @@
          buttonClick();
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
-     });
-     test("h1 should exist", () => {
-         expect(document.getElementsByTagName("h1").length).toBe(1);
      });
  });
